643b375e86f8ca7d064914c16fa1ff8daa20b1a0
nsidhu7 <132525478+nsidhu7@users.noreply.github.com>
2023-09-27 23:37:23 -0500

    loading image using glide lib

--- app/src/main/java/com/mobile/bitbybit/MainActivity.kt
+++ app/src/main/java/com/mobile/bitbybit/MainActivity.kt
@@ -2,10 +2,33 @@ package com.mobile.bitbybit
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.bumptech.glide.Glide
+import com.bumptech.glide.load.engine.DiskCacheStrategy
+import com.mobile.bitbybit.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        // On clicking button load images from internet
+        binding.button.setOnClickListener {
+
+            val url = "https://picsum.photos/200" //declaring url
+
+            //Pass the context of activity
+
+            Glide.with(this)
+                .load(url)
+                .fitCenter()
+                .skipMemoryCache(true)  // skip cache to load new pic
+                .diskCacheStrategy(DiskCacheStrategy.NONE)
+                .placeholder(R.drawable.placeholderimg) //while loading default image appears
+                .into(binding.imageView)
+
+        }
     }
 }
